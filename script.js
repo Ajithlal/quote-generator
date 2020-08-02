e09fc5ac6af8670c3d41d5ae3ce2ecd61261016d
Ajithlal Parackel <Ajithlal@users.noreply.github.com>
2020-08-02 05:46:36 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// show loading
+function complete() {
+    if (!loader.hidden) {
+        loader.hidden = true;
+        quoteContainer.hidden = false;
+    }
+}
+// Get quote from api.
+async function getQuote() {
+ loading();
+ const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+ const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+ try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // if author is blank add 'unknown'
+    if (data.quoteAuthor === '') {
+        authorText.innerText = 'unknown';
+    } else {
+        authorText.innerText = data.quoteAuthor;
+    }
+    console.log(data);
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    //stop loader and show quote
+    complete();
+ }catch(error) {
+     getQuote();
+     console.log('ayyo no quote', error);
+ }
+}
+//tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Even listener
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+// On Load
+getQuote();
